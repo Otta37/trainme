f404a454687019810f41d5304fa87109cb93fb5a
nicolorigoni03 <nicolo.rigoni4@gmail.com>
2023-12-29 17:56:34 +0100

    Prova commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Login from './login'
-import Home from './Home'
-import Schede from './Schede'
-import './App.css'
-import 'primereact/resources/themes/lara-light-cyan/theme.css'
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import { UtenteContext } from './stores/UtenteContext';
-import { useState } from 'react';
-import SkeletonHome from './SkeletonHome'
-import axios from 'axios';
+import Login from "./login";
+import Home from "./Home";
+import Schede from "./Schede";
+import "./App.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { UtenteContext } from "./stores/UtenteContext";
+import { useState } from "react";
+import SkeletonHome from "./SkeletonHome";
+import axios from "axios";
 
 function App() {
   const [auth, setAuth] = useState(false);
@@ -23,35 +28,37 @@ function App() {
   }, [location.pathname]); // Dipendenza dell'effetto sull'endpoint
 
   const checkAuth = () => {
-
     let token_jwt = localStorage.getItem("jwt");
+
+    let aaa = "";
 
     if (token_jwt) {
       axios.defaults.headers.common["Authorization"] = `Bearer ${token_jwt}`;
-      axios.get("http://192.168.1.27:3000/is_auth")
-        .then((res) => {
-          localStorage.setItem("jwt", res.data);
-          axios.defaults.headers.common["Authorization"] = `Bearer ${res.data}`;
-        });
+      axios.get("http://192.168.1.27:3000/is_auth").then((res) => {
+        localStorage.setItem("jwt", res.data);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${res.data}`;
+      });
     } else {
       delete axios.defaults.headers.common["Authorization"];
     }
-  }
-
+  };
 
   return (
     <>
-      <UtenteContext.Provider value={{auth, setAuth, index, setIndex}}>
+      <UtenteContext.Provider value={{ auth, setAuth, index, setIndex }}>
         <Router>
           <Routes>
             <Route path="/" index element={<Login />} />
             <Route path="/home" element={<SkeletonHome body={<Home />} />} />
-            <Route path="/schede" element={<SkeletonHome body={<Schede />} />} />
+            <Route
+              path="/schede"
+              element={<SkeletonHome body={<Schede />} />}
+            />
           </Routes>
         </Router>
       </UtenteContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
